aea85f97d3f27654b7eec9f701ef74ba5691fcb4
Nkwor-Jane <69125833+Nkwor-Jane@users.noreply.github.com>
2020-09-04 23:06:15 +0100

    Correction

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-
 const interestCalc = (principalAmount, lengthOfInvestment) => {
     if(lengthOfInvestment <= 1){
-        return ( `Your payment is ${(principalAmount * (1 + (lengthOfInvestment * 0.015)))} `)
+        return ( ` Your interest with us after ${lengthOfInvestment} years is ${Math.ceil(principalAmount * 0.015 * lengthOfInvestment)} and total  paable amount is ${ Math.ceil((principalAmount * (1 + (lengthOfInvestment * 0.015))))} `)
     }else if(lengthOfInvestment >= 3 && lengthOfInvestment <= 5){
-        return ( `Your payment is ${(principalAmount * (1 + (lengthOfInvestment * 0.025)))} `)
+        return ( `Your interest with us after ${lengthOfInvestment} years is ${Math.ceil(principalAmount * 0.025 * lengthOfInvestment)} and total payable amount is ${Math.ceil((principalAmount * (1 + (lengthOfInvestment * 0.025))))} `)
     }else if (lengthOfInvestment  < 10){
-        return ( `Your payment is ${(principalAmount * (1 + (lengthOfInvestment * 0.035)))} `)
+        return ( `Your interest with us after ${lengthOfInvestment} years is ${Math.ceil(principalAmount * 0.035 * lengthOfInvestment)} and total payable amount is ${Math.ceil((principalAmount * (1 + (lengthOfInvestment * 0.035))))} `)
     }else {
-        return ( `Your payment is ${(principalAmount * (1 + (lengthOfInvestment * 0.05)))} `)
-    }
-}
+        return ( `Your interest with us after ${lengthOfInvestment} years is ${Math.ceil(principalAmount * 0.05 * lengthOfInvestment)} and total payable amount is ${Math.ceil((principalAmount * (1 + (lengthOfInvestment * 0.05))))} `)
+    }}
 
-console.log(`Your interest with us after is 6050 and total payable amount is ${interestCalc(11000, 11)}`);
-console.log(`Your interest with us after is 3080 and total payable amount is ${interestCalc(11000, 8)}`);
-console.log(`Your interest with us after is 1000 and total payable amount is ${interestCalc(10000, 4)}`);
-console.log(`Your interest with us after is 150 and total payable amount is ${interestCalc(10000, 1)}`);
+
+   
+console.log(interestCalc(11000, 11));
+console.log(interestCalc(11000, 8));
+console.log(interestCalc(10000, 4));
+console.log(interestCalc(10000, 1));
+console.log(interestCalc(50000, 10))

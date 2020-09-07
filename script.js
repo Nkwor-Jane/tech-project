553cd0f5416bb0a00ea3ce022468ea21ef3586a2
Nkwor-Jane <69125833+Nkwor-Jane@users.noreply.github.com>
2020-09-07 21:21:58 +0100

    The correction for years

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-const interestCalc = (principalAmount, lengthOfInvestment) => {
+ const interestCalc = (principalAmount, lengthOfInvestment) => {
+
+    let years = (lengthOfInvestment) =>{
+    if(lengthOfInvestment === 1){
+        return(` ${lengthOfInvestment} year`)
+    }else if(lengthOfInvestment > 1 ){
+        return (`${lengthOfInvestment} years`)
+    }else{
+        return(`${lengthOfInvestment} months`)
+    }
+}
+
     if(lengthOfInvestment <= 1){
-        return ( ` Your interest with us after ${lengthOfInvestment} years is ${Math.ceil(principalAmount * 0.015 * lengthOfInvestment)} and total  paable amount is ${ Math.ceil((principalAmount * (1 + (lengthOfInvestment * 0.015))))} `)
+        return ( ` Your interest with us after ${years(lengthOfInvestment)} is ${Math.ceil(principalAmount * 0.015 * lengthOfInvestment)} and the total  payable amount is ${ Math.ceil((principalAmount * (1 + (lengthOfInvestment * 0.015))))} `)
     }else if(lengthOfInvestment >= 3 && lengthOfInvestment <= 5){
-        return ( `Your interest with us after ${lengthOfInvestment} years is ${Math.ceil(principalAmount * 0.025 * lengthOfInvestment)} and total payable amount is ${Math.ceil((principalAmount * (1 + (lengthOfInvestment * 0.025))))} `)
+        return ( `Your interest with us after ${years(lengthOfInvestment)} is ${Math.ceil(principalAmount * 0.025 * lengthOfInvestment)} and the total payable amount is ${Math.ceil((principalAmount * (1 + (lengthOfInvestment * 0.025))))} `)
     }else if (lengthOfInvestment  < 10){
-        return ( `Your interest with us after ${lengthOfInvestment} years is ${Math.ceil(principalAmount * 0.035 * lengthOfInvestment)} and total payable amount is ${Math.ceil((principalAmount * (1 + (lengthOfInvestment * 0.035))))} `)
+        return ( `Your interest with us after ${years(lengthOfInvestment)} is ${Math.ceil(principalAmount * 0.035 * lengthOfInvestment)} and the total payable amount is ${Math.ceil((principalAmount * (1 + (lengthOfInvestment * 0.035))))} `)
     }else {
-        return ( `Your interest with us after ${lengthOfInvestment} years is ${Math.ceil(principalAmount * 0.05 * lengthOfInvestment)} and total payable amount is ${Math.ceil((principalAmount * (1 + (lengthOfInvestment * 0.05))))} `)
-    }}
+        return ( `Your interest with us after ${years(lengthOfInvestment)} is ${Math.ceil(principalAmount * 0.05 * lengthOfInvestment)} and the total payable amount is ${Math.ceil((principalAmount * (1 + (lengthOfInvestment * 0.05))))} `)
+    }
+}
+
 
 
    

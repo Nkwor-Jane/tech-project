82f4f860a58dbbaad04e317ce5ae19c0d421510f
Nkwor-Jane <69125833+Nkwor-Jane@users.noreply.github.com>
2020-09-16 12:21:00 +0100

    Still Unsolved

---
+++ task3.js
@@ -0,0 +1,25 @@
+
+// const sortArray = (a,b) => a.concat(b).sort()
+
+
+
+// const  sortArray = (arr) =>{
+//     let newArray = []
+//     arr.reduce((newArr,item) =>{
+//         newArr.push(item)
+//         return newArray
+//     }), arr
+// } 
+
+// const mergeArray = (arr) =>{
+//     return [...new Set([].concat(...arr).sort()]
+// }
+
+
+
+console.log(mergeArray([6,3,4,0,-1], [4,2,7,4,1,0],[0,2,7,3,4,1,8,-2]))
+
+// console.log(sortArray([1,2,3,5,4,0], [0,2,7,3,4,1,8,-2]))
+
+// console.log(sortArray( [2,3,1,5,4], []))
+// console.log(sortArray([], [2,3,1,5,4]))
